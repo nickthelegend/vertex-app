703b66d857059578f1662cc4151dcda25b4d6dac
nickthelegend <testingtesla7@gmail.com>
2024-06-20 08:13:28 +0400

    All Images Are Given and Updated OrderFood

--- screens/OrderFood.js
+++ screens/OrderFood.js
@@ -45,13 +45,13 @@ export const bakeryItems = [
 {
 
   name :  "Veg Puff",
-  image: "",
+  image: require("../images/canteen/Veg Puff.jpeg"),
   price : 20
 } , 
 
 
 { name: "Egg Puff",
-  image : "",
+  image : require("../images/canteen/Egg Puff.jpeg"),
   price : 25
 
 
@@ -59,15 +59,15 @@ export const bakeryItems = [
 },
 
 { name: "Chicken Puff",
-  image : "",
+  image : require("../images/canteen/Chicken Puff.jpeg"),
   price : 30
 
 
 
 },
 
-{ name: "Panner Puff",
-  image : "",
+{ name: "Paneer Puff",
+  image : require("../images/canteen/Paneer Puff.jpeg"),
   price : 30
 
 
@@ -75,7 +75,7 @@ export const bakeryItems = [
 },
 
 { name: "Gone Mad",
-  image : "",
+  image : require("../images/canteen/Gone mad.jpeg"),
   price : 5
 
 
@@ -84,7 +84,7 @@ export const bakeryItems = [
 
 
 { name: "Hershey's ChocoTubes",
-  image : "",
+  image : require("../images/canteen/Hersheys choco tube (chocolate).jpeg"),
   price : 20
 
 
@@ -94,7 +94,7 @@ export const bakeryItems = [
 
 
 { name: "Chocolate Dips",
-  image : "",
+  image : require("../images/canteen/chocolatedips.jpg"),
   price : 10
 
 
@@ -103,7 +103,7 @@ export const bakeryItems = [
 
 
 { name: "Dilkush",
-  image : "",
+  image : require("../images/canteen/dilkush.jpg"),
   price : 20
 
 
@@ -112,7 +112,7 @@ export const bakeryItems = [
 
 
 { name: "Dilpasand",
-  image : "",
+  image : require("../images/canteen/Dil pasand.jpg"),
   price : 20
 
 
@@ -122,7 +122,7 @@ export const bakeryItems = [
 
 
 { name: "Cream Bun",
-  image : "",
+  image : require("../images/canteen/cream bun.jpg"),
   price : 20
 
 
@@ -134,7 +134,7 @@ export const bakeryItems = [
 
 
 { name: "Sandwich",
-  image : "",
+  image :  require("../images/canteen/sandwich.jpg"),
   price : 30
 
 
@@ -145,7 +145,7 @@ export const bakeryItems = [
 
 
 { name: "Samosa [Big]",
-  image : "",
+  image :  require("../images/canteen/samosabig.jpg"),
   price : 20
 
 
@@ -153,7 +153,7 @@ export const bakeryItems = [
 },
 
 { name: "Samosa [Small] 1 Plate",
-  image : "",
+  image : require("../images/canteen/samosasmall.jpg"),
   price : 30
 
 
@@ -166,68 +166,68 @@ export const bakeryItems = [
 
 
 
-const nonVegItems = [
-  "Chicken Curry",
-  "Fish Fry",
-  "Mutton Biryani",
-  "Prawn Fry",
-];
+// const nonVegItems = [
+//   "Chicken Curry",
+//   "Fish Fry",
+//   "Mutton Biryani",
+//   "Prawn Fry",
+// ];
 const iceCreamItems = ["Vanilla", "Chocolate", "Strawberry", "Butterscotch"];
 const coolDrinksItems = [
   { name: "Thumps Up",
-  image : "",
+  image : require("../images/canteen/Thumbs Up.jpg"),
   price : 20,
 },
 
 { name: "Sprite",
-  image : "",
+  image : require("../images/canteen/Sprite.png"),
   price : 20,
 },
 
 { name: "CocaCola",
-  image : "",
+  image : require("../images/canteen/CocaCola.jpeg"),
   price : 20,
 },
 
 { name: "Fanta",
-  image : "",
+  image : require("../images/canteen/Fanta.jpg"),
   price : 20,
 },
 
 { name: "Limca",
-  image : "",
+  image : require("../images/canteen/Limca.jpeg"),
   price : 20,
 },
 
 { name: "Mountain Dew",
-  image : "",
+  image : require("../images/canteen/Mountain Dew.jpeg"),
   price : 20,
 },
 
 { name: "Water Bottle",
-  image : "",
+  image : require("../images/canteen/waterbottle.jpg"),
   price : 20,
 },
 
 
 { name: "Minutemaid Pulpy Orange",
-  image : "",
+  image : require("../images/canteen/Minute maid-pulpy orange.jpeg"),
   price : 20,
 },
 
 { name: "Maaza [Tetrapack]",
-  image : "",
+  image : require("../images/canteen/Maaza Tetra pack.jpeg"),
   price : 10,
 },
 
 
 { name: "Maaza [Bottle]",
-  image : "",
+  image : require("../images/canteen/Maaza bottle.jpeg"),
   price : 25,
 },
 
 { name: "Minutemaid Grape Juice",
-  image : "",
+  image : require("../images/canteen/Minute maid- Grape juice.jpeg"),
   price : 20,
 },
 
@@ -243,59 +243,64 @@ export const fastFoodItems = [
 
 
   { name: "Veg Manchuria",
-  image : "",
+  image : require("../images/canteen/veg manchuria.jpg"),
   price : 60,
 },
 
 { name: "Veg Noodles",
-  image : "",
+  image : require("../images/canteen/veg noodles.jpeg"),
   price : 60,
 },
 
 
 { name: "Veg FriedRice",
-  image : "",
+  image : require("../images/canteen/veg fried rice.webp"),
   price : 50,
 },
 
 { name: "Veg Manchurian Fried Rice",
-  image : "",
+  image : require("../images/canteen/Veg Manchurian Fried Rice.jpg"),
   price : 60,
 },
 
 {  name: "Veg Manchurian Noodles",
-  image : "",
+  image : require("../images/canteen/Veg Manchurian Noodles.jpg"),
   price : 70,
 },
 {  name: "Egg Fried Rice",
-  image : "",
+  image : require("../images/canteen/egg fried rice.jpg"),
   price : 60,
 },
 
 
 {  name: "Double Egg Fried Rice",
-  image : "",
+  image : require("../images/canteen/Double Egg Fried Rice.jpg"),
   price : 70,
 },
 
 {  name: "Chicken Fried Rice",
-  image : "",
+  image : require("../images/canteen/chicken fried rice.jpg"),
   price : 70,
 },
 
 {  name: "Double Egg Chicken Fried Rice",
-  image : "",
+  image : require("../images/canteen/Double Egg Chicken Fried Rice.jpg"),
+  price : 80,
+},
+
+{  name: "Chicken Noodles",
+  image : require("../images/canteen/chicken_noodles.jpg"),
   price : 80,
 },
 
 {  name: "Double Egg Chicken Noodles",
-  image : "",
+  image : require("../images/canteen/chicken noodles.jpg"),
   price : 80,
 },
 
 
 {  name: "Veg Manchurian Noodles",
-  image : "",
+  image : require("../images/canteen/veg manchuria.jpg"),
   price : 70,
 },
 ]
@@ -305,50 +310,50 @@ export const fastFoodItems = [
 
 export const fruitJuices = [
     {  name: "Mosambi",
-  image : "",
+  image : require("../images/canteen/Mosambi.jpg"),
   price : 40,
 },
 
 {  name: "Grapes",
-  image : "",
+  image : require("../images/canteen/Grapes.jpg"),
   price : 40,
 },
 
 {  name: "Sappota",
-  image : "",
+  image : require("../images/canteen/Sappota.jpg"),
   price : 30,
 },
 
 {  name: "Muskmelon",
-  image : "",
+  image : require("../images/canteen/Muskmelon.jpg"),
   price : 30,
 },
 
 
 
   {  name: "Watermelon",
-  image : "",
+  image : require("../images/canteen/Watermelon.jpg"),
   price : 30,
 },
 
 {  name: "Papaya",
-  image : "",
+  image : require("../images/canteen/Papaya.jpg"),
   price : 30,
 },
 {  name: "Pineapple",
-  image : "",
+  image : require("../images/canteen/Pineapple.jpg"),
   price : 70,
 },
 
 
 {  name: "Mango",
-  image : "",
+  image : require("../images/canteen/Mango.jpg"),
   price : 30,
 },
 
 
 {  name: "Fruit Bowl",
-  image : "",
+  image : require("../images/canteen/Fruit Bowl.jpg"),
   price : 50,
 },
 ]
@@ -356,37 +361,37 @@ export const fruitJuices = [
 export const frankiesItems = [
 
   {  name: "Tangi Paneer Franike",
-  image : "",
+  image : require("../images/canteen/Tangi Paneer Franike.jpg"),
   price : 70,
 },
 
 {  name: "Paneer Tikka Franike",
-image : "",
+image : require("../images/canteen/Paneer Tikka Franike.jpg"),
 price : 70,
 },
 {  name: "Kadai Paneer Franike",
-image : "",
+image : require("../images/canteen/Kadai Paneer Franike.jpg"),
 price : 70,
 },
 
 {  name: "Veg Manchurian Franike",
-image : "",
+image : require("../images/canteen/Veg Manchurian Franike.jpg"),
 price : 70,
 },
 
 {  name: "Schezwan Veg Frankie",
-image : "",
+image : require("../images/canteen/Schezwan Veg Frankie.jpg"),
 price : 70,
 
 },
 
-{  name: "Hyderabadi Chicken Franike",
-image : "",
+{  name: "Hyderabadi Chicken Frankie",
+image : require("../images/canteen/Hyderabadi Chicken Franike.jpg"),
 price : 80,
 },
 
 {  name: "Chicken Tikka Franike",
-image : "",
+image : require("../images/canteen/Chicken Tikka Franike.jpg"),
 price : 80,
 },
 
@@ -395,41 +400,41 @@ price : 80,
 
 
 export const snacksItems = [ {  name: "French Fries",
-image : "",
+image : require("../images/canteen/French Fries.jpg"),
 price : 60,
 },
 
 {  name: "Potato Twister",
-image : "",
+image : require("../images/canteen/Potato Twister.jpg"),
 price : 60,
 },
 
 
 {  name: "Veg Chilli Garlic Bites [10 pcs]",
-image : "",
+image : require("../images/canteen/Veg Chilli Garlic Bites.jpg"),
 price : 60,
 },
 
 
 
 {  name: "Egg Stick ",
-image : "",
+image : require("../images/canteen/Egg Stick.jpg"),
 price :40,
 },
 
 
 {  name: "Chicken PopCorn [10pcs]",
-image : "",
+image : require("../images/canteen/Chicken PopCorn.jpg"),
 price : 70,
 },
 
 {  name: "Chicken Nuggets [5pcs]",
-image : "",
+image : require("../images/canteen/Chicken Nuggets.jpg"),
 price : 60,
 },
 
 {   name: "Chicken Cheese Balls [7pcs]",
-image : "",
+image : require("../images/canteen/Chicken Cheese Balls.jpg"),
 price : 85,
 },
 
@@ -449,26 +454,37 @@ price : 85,
 
 
 export default function OrderFood({ navigation }) {
-  const [selectedCategory, setSelectedCategory] = useState("Veg");
-
+  const [selectedCategory, setSelectedCategory] = useState("Bakery");
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleSearch = (text) => {
+    setSearchQuery(text);
+  };
   let itemsToDisplay;
   switch (selectedCategory) {
-    case "Veg":
-      itemsToDisplay = vegItems;
+    case "Bakery":
+      itemsToDisplay = bakeryItems;
       break;
-    case "Non Veg":
-      itemsToDisplay = nonVegItems;
+    case "FastFood":
+      itemsToDisplay = fastFoodItems;
       break;
-    case "IceCream":
-      itemsToDisplay = iceCreamItems;
+    case "FruitJuice":
+      itemsToDisplay = fruitJuices;
       break;
+    case "Snacks":
+        itemsToDisplay = snacksItems;
+        break;
     case "Cool Drinks":
       itemsToDisplay = coolDrinksItems;
       break;
+    case "Frankie":
+      itemsToDisplay = frankiesItems;
+      break;  
     default:
       itemsToDisplay = [];
   }
-
+  itemsToDisplay = itemsToDisplay.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   // Function to chunk the array into arrays of two items each
   const chunkArray = (array, chunkSize) => {
     return Array(Math.ceil(array.length / chunkSize))
@@ -480,7 +496,7 @@ export default function OrderFood({ navigation }) {
   // Chunk the items into arrays of two
   const itemsInRows = chunkArray(itemsToDisplay, 2);
   const [cartItemCount, setCartItemCount] = useState(0);
-
+  
   useEffect(() => {
     getCartItemCount();
   }, []);
@@ -535,77 +551,121 @@ export default function OrderFood({ navigation }) {
         </View>
       <ScrollView style={styles.container}>
         
-        <View
-          style={{
-            flex: 1,
-            borderRadius: 15,
-            backgroundColor: "#efefef",
-            padding: 10,
-            margin: 10,
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <Ionicons
-            name="search"
-            size={24}
-            color="#aaa"
-            style={{ marginRight: 10 }}
-          />
-          <TextInput
-            placeholder="What food is on your mind?"
-            style={{ flex: 1, fontSize: 18 }} // Adjust the width of TextInput to take remaining space
-          />
-        </View>
+      <View
+  style={{
+    flex: 1,
+    borderRadius: 15,
+    backgroundColor: "#efefef",
+    padding: 10,
+    margin: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  }}
+>
+  <Ionicons
+    name="search"
+    size={24}
+    color="#aaa"
+    style={{ marginRight: 10 }}
+  />
+  <TextInput
+    placeholder="What food is on your mind?"
+    style={{ flex: 1, fontSize: 18 }}
+    value={searchQuery}
+    onChangeText={handleSearch}
+  />
+</View>
         <ScrollView contentContainerStyle={styles.categoriesContainer} horizontal showsHorizontalScrollIndicator={false} style={{padding:10}}>
           <TouchableOpacity
-            onPress={() => setSelectedCategory("Veg")}
+            onPress={() => setSelectedCategory("Bakery")}
             style={[
               styles.categoryButton,
-              selectedCategory === "Veg" && styles.categoryButtonSelected,
+              selectedCategory === "Bakery" && styles.categoryButtonSelected,
             ]}
           >
             <Text
               style={[
                 styles.categoryText,
-                selectedCategory === "Veg" && styles.categoryTextSelected,
+                selectedCategory === "Bakery" && styles.categoryTextSelected,
               ]}
             >
               Bakery & Confectionery
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => setSelectedCategory("Non Veg")}
+            onPress={() => setSelectedCategory("FastFood")}
             style={[
               styles.categoryButton,
-              selectedCategory === "Non Veg" && styles.categoryButtonSelected,
+              selectedCategory === "FastFood" && styles.categoryButtonSelected,
             ]}
           >
             <Text
               style={[
                 styles.categoryText,
-                selectedCategory === "Non Veg" && styles.categoryTextSelected,
+                selectedCategory === "FastFood" && styles.categoryTextSelected,
               ]}
             >
-              Non Veg
+              Fast Food
             </Text>
           </TouchableOpacity>
+
+
           <TouchableOpacity
-            onPress={() => setSelectedCategory("IceCream")}
+            onPress={() => setSelectedCategory("Frankie")}
             style={[
               styles.categoryButton,
-              selectedCategory === "IceCream" && styles.categoryButtonSelected,
+              selectedCategory === "Frankie" &&
+                styles.categoryButtonSelected,
             ]}
           >
             <Text
               style={[
                 styles.categoryText,
-                selectedCategory === "IceCream" && styles.categoryTextSelected,
+                selectedCategory === "Frankie" &&
+                  styles.categoryTextSelected,
               ]}
             >
-              IceCream
+              Frankies
             </Text>
           </TouchableOpacity>
+          
+
+          <TouchableOpacity
+            onPress={() => setSelectedCategory("Snacks")}
+            style={[
+              styles.categoryButton,
+              selectedCategory === "Snacks" && styles.categoryButtonSelected,
+            ]}
+          >
+            <Text
+              style={[
+                styles.categoryText,
+                selectedCategory === "Snacks" && styles.categoryTextSelected,
+              ]}
+            >
+              Snacks
+            </Text>
+          </TouchableOpacity>
+
+
+
+          <TouchableOpacity
+            onPress={() => setSelectedCategory("FruitJuice")}
+            style={[
+              styles.categoryButton,
+              selectedCategory === "FruitJuice" && styles.categoryButtonSelected,
+            ]}
+          >
+            <Text
+              style={[
+                styles.categoryText,
+                selectedCategory === "FruitJuice" && styles.categoryTextSelected,
+              ]}
+            >
+              Fruit Juice
+            </Text>
+          </TouchableOpacity>
+
           <TouchableOpacity
             onPress={() => setSelectedCategory("Cool Drinks")}
             style={[
@@ -642,13 +702,13 @@ export default function OrderFood({ navigation }) {
                     source={item.image}
                     style={[styles.itemImage, { borderRadius: 50 }]}
                   >
-                    <TouchableOpacity
+                    {/* <TouchableOpacity
                       style={[styles.button, styles.favoriteButton]}
                     >
                       <Ionicons name="heart-outline" size={21} color="white" />
-                    </TouchableOpacity>
+                    </TouchableOpacity> */}
                   </ImageBackground>
-                  <Text style={styles.itemText}>{item.name}</Text>
+                  <Text style={styles.itemText} numberOfLines={1}>{item.name}</Text>
                   <Text style={styles.itemPrice}>₹{item.price}</Text>
                   <View style={styles.buttonContainer}>
                     <TouchableOpacity
@@ -780,7 +840,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   rowContainer: {
-    flexDirection: "row",
+    // flexDirection: "row",
     marginBottom: 10,
   },
   itemContainer: {
@@ -800,19 +860,23 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     paddingBottom: 20,
     paddingTop: 20,
+    marginBottom:20
   },
   itemImage: {
     width: (screenWidth / 2) * 0.85,
     height: 150,
     borderRadius: 40,
-    resizeMode: "cover",
+    resizeMode: "contain",
     flexDirection: "row",
   },
   itemText: {
-    fontSize: 18,
+    fontSize: 16,
     fontFamily: "ComfortaaBold",
     marginTop: 10,
     color: "#007bff",
+    textAlign:'center',
+    width:'50%',
+    // textAlign:'center'
   },
   itemPrice: {
     fontSize: 35,
