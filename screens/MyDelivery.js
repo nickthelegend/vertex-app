c91c901c9c5ea2f8f557d510064db216e77cac85
nickthelegend <testingtesla7@gmail.com>
2024-06-24 20:20:36 +0400

    Header added for MyOrders and MyDelivery

--- screens/MyDelivery.js
+++ screens/MyDelivery.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
 
 export default function MyDelivery() {
+
+    const navigation = useNavigation();
+
   return (
-    <View>
-      <Text></Text>
-     </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+      <TouchableOpacity onPress={()=>{navigation.goBack()}} style={styles.backButton}>
+          <Ionicons name="arrow-back" size={30} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.headerText}>My Deliveries</Text>
+        <View></View>
+      </View>
+     </SafeAreaView>
   );
 }
+
+
+const styles = StyleSheet.create({
+
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 15,
+        backgroundColor: '#ffffff',
+        borderBottomWidth: 1,
+        borderBottomColor: '#dddddd',
+        justifyContent: 'space-between',
+      },
+      backButton: {
+        marginRight: 10,
+      },
+      headerText: {
+        fontSize: 25,
+        fontFamily: 'ComfortaaBold',
+      },
+
+})
